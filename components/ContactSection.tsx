96f37cd5da6c63abf4cf156483b6249c1ddd1219
DimasCaka <dimascakaramanda01@gmail.com>
2023-06-19 16:07:20 +0800

    menambahkan style pada contact

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,17 +1,80 @@
-import React from 'react'
-import Image from 'next/image'
+"use client"
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
-const ContactSection = () => {
-  return (
-    <section id='contact'>
-        <div className="my-12 pb-12 md:pt-16 md:pb-48">
-            <h1 className="text-center font-bold text-4xl">
-            Contact Me
-            <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
-            </h1>
+function ContactForm(){
+  const [state, handleSubmit] = useForm("mayzrlbb");
+  if(state.succeeded){
+    return <p>test</p>;
+  }
+  return(
+  <section id='contact'>
+  <div className="my-12 pb-12 md:pt-16 md:pb-48">
+      <h1 className="text-center font-bold text-4xl">
+      Contact Me
+      <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+      <div className='block max-w-md rounded-lg bg-cyan-950 p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 mx-auto'>
+        <form className='' onSubmit={handleSubmit}>
+        <div className="relative mb-6" data-te-input-wrapper-init>
+          <input
+            type="text"
+            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            id="name"
+            name='name'
+            placeholder="Name" />
+          <label
+            htmlFor="name"
+            className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+            >Name
+          </label>
+          <ValidationError 
+              prefix="Name" 
+              field="name"
+              errors={state.errors}
+            />
         </div>
-    </section>
-  )
+          <div className='relative mb-6'>
+            <input className='peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0'
+              id="email"
+              type="email" 
+              name="email"
+            />
+            <label className='pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary' htmlFor="email">
+              Email Address
+            </label>
+            <ValidationError 
+              prefix="Email" 
+              field="email"
+              errors={state.errors}
+            />
+          </div>
+          <div className='relative mb-6'>
+            <textarea className='peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0'
+              id="message"
+              name="message"
+              placeholder='Message'
+            />
+            <label className='pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary' htmlFor="message">Message</label>
+            <ValidationError 
+              prefix="Message" 
+              field="message"
+              errors={state.errors}
+            />
+          </div>
+          <button className='dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]] inline-block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]'
+          type="submit" disabled={state.submitting}>
+            Submit
+          </button>
+      </form>
+      </div>
+  </div>
+</section>
+)
 }
-
-export default ContactSection
+function App(){
+  return (
+    <ContactForm/>
+  );
+}
+export default App;
