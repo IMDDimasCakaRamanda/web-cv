8b7600ec7824004ac19b0933c0b5d7900e50c1bb
DimasCaka <dimascakaramanda01@gmail.com>
2023-06-23 09:11:49 +0800

    Revisi 3

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -15,7 +15,7 @@ const skills = [
 const AboutSection = () => {
   return (
     <section id="about">
-      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+      <div className="my-40 pb-12 md:pt-16 md:pb-48">
         <h1 className="text-center font-bold text-4xl">
           About Me
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
@@ -35,10 +35,6 @@ const AboutSection = () => {
             <br />
             <p>
             I aim to become a proficient software engineer by applying my theoretical understanding to real-world projects. My goal is to contribute to the advancement of technology through innovative software solutions.
-            </p>
-            <br />
-            <p>
-            As a student in the Software Engineering program, I strive to develop a strong foundation in programming languages, algorithms, and software design principles. My objective is to excel in problem-solving and create efficient and user-friendly applications.
             </p>
             <br />
             <p>
--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -24,7 +24,7 @@ const projects = [
 
 const ProjectsSection = () => {
   return (
-    <section id="projects">
+    <section className="my-20" id="projects">
       <h1 className="my-10 text-center font-bold text-4xl">
         Projects
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
