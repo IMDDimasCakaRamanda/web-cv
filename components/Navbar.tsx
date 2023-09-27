50dfb9d1004975eb39786b22873ca9f7b65b130b
DimasCaka <dimascakaramanda01@gmail.com>
2023-09-27 10:15:22 +0800

    minor bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,6 @@ import React from "react"
 import { useState } from "react"
 import { Link } from "react-scroll/modules"
 import { usePathname } from "next/navigation"
-import { useTheme } from "next-themes"
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
 
 interface NavItem {
   label: string
